839f0e8f27056f119a0230fc0edcf139adc61e39
Carlos Juarez <carlosgj94@gmail.com>
2023-11-12 19:09:04 +0300

    chore: formatting contracts

--- src/interfaces/IExecutablePlugin.sol
+++ src/interfaces/IExecutablePlugin.sol
@@ -2,9 +2,9 @@
 pragma solidity >=0.8.21;
 
 abstract contract IExecutablePlugin {
-  function canExecute(uint256 _proposalId) external view virtual returns (bool);
+    function canExecute(uint256 _proposalId) external view virtual returns (bool);
 
-  function proposalCount() external view virtual returns (uint256);
+    function proposalCount() external view virtual returns (uint256);
 
-  function execute(uint256 _proposalId) external virtual;
+    function execute(uint256 _proposalId) external virtual;
 }
